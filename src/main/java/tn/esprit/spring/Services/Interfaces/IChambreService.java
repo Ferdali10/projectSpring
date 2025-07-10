5d76db52231faa72ceaa0b2843f2121e6b982172
mohamed ali ferchichi <148057675+Ferdali10@users.noreply.github.com>
2025-07-10 15:16:50 +0100

    Update IChambreService.java

--- src/main/java/tn/esprit/spring/Services/Interfaces/IChambreService.java
+++ src/main/java/tn/esprit/spring/Services/Interfaces/IChambreService.java
@@ -5,8 +5,12 @@ import tn.esprit.spring.Entity.Chambre;
 import java.util.List;
 
 public interface IChambreService {
+    
+    // Modification de test pour le webhook Jenkins
     List<Chambre> retrieveAllChambres();
+    
     Chambre addChambre(Chambre c);
     Chambre updateChambre (Chambre c);
     Chambre retrieveChambre (long idChambre);
+    
 }
