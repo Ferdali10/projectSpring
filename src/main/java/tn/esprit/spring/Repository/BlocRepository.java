4b63f851155fc06329c7df2e979808795aa8c6e5
mohamed ali ferchichi <148057675+Ferdali10@users.noreply.github.com>
2025-07-23 12:12:55 +0100

    Update BlocRepository.java

--- src/main/java/tn/esprit/spring/Repository/BlocRepository.java
+++ src/main/java/tn/esprit/spring/Repository/BlocRepository.java
@@ -3,7 +3,7 @@ package tn.esprit.spring.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tn.esprit.spring.Entity.Bloc;
 
-import java.util.List;
+
 
 
 public interface BlocRepository extends JpaRepository<Bloc, Long> {
